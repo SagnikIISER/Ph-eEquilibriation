b47f9f1afdab8316b70f75f5836dfb52d5b42571
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2020-12-28 11:44:37 +0530

    Update RajdeepEuler.c

--- RajdeepEuler.c
+++ RajdeepEuler.c
@@ -22,22 +22,24 @@ float main() {
   	float tone, ttwo, tthree;		/*extra variables*/
   	float h;										/*gap in time, "the epsilon"*/
 
-		int n;											/*array dimension*/
-		float omega, k;							/*omega, k; materials for dispersion relation*/
+		int n, k, ktot;							/*array dimension*/
+		float omega;							  /*omega, k; materials for dispersion relation*/
 		float lambda;								/*perturbation strenght parameter*/
 
 
 /*Working around the input function call*/
 
 	a= 0.0;										/*Read value of a*/
-	b= 50.0;									/*Read value of b*/
+	b= 10.0;									/*Read value of b*/
 	h= 0.1;										/*Read value of h*/
 	omega= 1;									/*Read value of omega*/
 	lambda=1;									/*Read value of lambda*/
 
-  n=(b-a)/h+1;								/*array Dimension*/
+  n=(b-a)/h+1;							/*array Dimension*/
+	ktot=20;									/*array Dimension*/
 
-/*	printf("the matrix dimension is:%d\n",n );        */
+	k=1;											/*Dummy Array*/
+/*	printf("the matrix dimension is:%d\n",n );   */
 
   tprime = a;
 
@@ -59,20 +61,20 @@ Defining Iteration variables and outputs
 DR, BarDR and the integration I
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-float DR[n][n];
-float BarDR[n][n];
+float DR[ktot][n][n];
+float BarDR[ktot][n][n];
 float IR[n][n];
 
 
-float DA[n][n];
-float BarDA[n][n];
+float DA[ktot][n][n];
+float BarDA[ktot][n][n];
 
 
-float DK[n][n];
-float BarDK[n][n];
+float DK[ktot][n][n];
+float BarDK[ktot][n][n];
 
-float IK1[n][n];
-float IK2[n][n];
+float	DUMMY[n][n];
+float IK[n][n];
 
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -83,11 +85,16 @@ The Module for Dyson Iteration using Euler Method:
 
 for (t=a; t<=b; t=t+h){
 
-IR[(int)(t/h)][(int)(t/h)]=0;
-DR[(int)(t/h)][(int)(t/h)]=0;
-BarDR[(int)(t/h)][(int)(t/h)]= 1;
 
-DR[(int)((t+h)/h)][(int)(t/h)]= DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(t/h)];
+IR[(int)(t/h)][(int)(t/h)]=0;
+DR[k][(int)(t/h)][(int)(t/h)]=0;
+BarDR[k][(int)(t/h)][(int)(t/h)]= 1;
+
+IK[(int)(t/h)][(int)(t/h)]=0;
+DUMMY[(int)(t/h)][(int)(t/h)]=0;
+
+
+DR[k][(int)((t+h)/h)][(int)(t/h)]= DzeroR(omega,t+h,t)*BarDR[k][(int)(t/h)][(int)(t/h)];
 IR[(int)((t+h)/h)][(int)(t/h)]=0;
 
 }
@@ -99,13 +106,13 @@ int i;
 
 	for (tprime=a; tprime<=b; tprime=tprime+h){
 	for (t=tprime+h; t<=b; t=t+h)
-	 {BarDR[(int)(t/h)][(int)(tprime/h)]=  BarDzeroR(omega,t,t-h)*BarDR[(int)((t-h)/h)][(int)(tprime/h)]-omega*omega*DzeroR(omega,t,t-h)*DR[(int)((t-h)/h)][(int)(tprime/h)]+(h/2)*BarDzeroR(omega,t,t-h)*IR[(int)((t-h)/h)][(int)(tprime/h)];
-		DR[(int)((t+h)/h)][(int)(tprime/h)]= DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(tprime/h)]+BarDzeroR(omega,t+h,t)*DR[(int)(t/h)][(int)(tprime/h)]+(h/2)*DzeroR(omega,t+h,t)*IR[(int)(t/h)][(int)(tprime/h)];
+	 {BarDR[k][(int)(t/h)][(int)(tprime/h)]=  BarDzeroR(omega,t,t-h)*BarDR[k][(int)((t-h)/h)][(int)(tprime/h)]-omega*omega*DzeroR(omega,t,t-h)*DR[k][(int)((t-h)/h)][(int)(tprime/h)]+(h/2)*BarDzeroR(omega,t,t-h)*IR[(int)((t-h)/h)][(int)(tprime/h)];
+		DR[k][(int)((t+h)/h)][(int)(tprime/h)]= DzeroR(omega,t+h,t)*BarDR[k][(int)(t/h)][(int)(tprime/h)]+BarDzeroR(omega,t+h,t)*DR[k][(int)(t/h)][(int)(tprime/h)]+(h/2)*DzeroR(omega,t+h,t)*IR[(int)(t/h)][(int)(tprime/h)];
 
 
 				for (i = (int)((tprime+h)/h); i <= (int)((t)/h); i++)
 				{
-					P=P+h*SigmaR(omega,t+h,(h*i))*DR[i][(int)(tprime/h)];
+					P=P+h*SigmaR(omega,t+h,(h*i))*DR[k][i][(int)(tprime/h)];
 				}
 		IR[(int)((t+h)/h)][(int)(tprime/h)]=P;
 		P=0.0;
@@ -117,12 +124,43 @@ int i;
 
 for (tprime=a; tprime<=b; tprime=tprime+h){
 for (t=tprime+h; t<=b; t=t+h){
-		DA[(int)((tprime)/h)][(int)(t/h)]=DR[(int)((t)/h)][(int)(tprime/h)];
-		BarDA[(int)((tprime)/h)][(int)(t/h)]=BarDR[(int)((t)/h)][(int)(tprime/h)];
+		DA[k][(int)((tprime)/h)][(int)(t/h)]=DR[k][(int)((t)/h)][(int)(tprime/h)];
+		BarDA[k][(int)((tprime)/h)][(int)(t/h)]=BarDR[k][(int)((t)/h)][(int)(tprime/h)];
 }
 }
 
 /*The Keldysh Part*/
+
+
+
+
+
+
+for (t=a; t<=b; t=t+h){
+for (tprime=a; tprime<=b; tprime=tprime+h){
+
+	for (i = (int)((a+h)/h); i <= (int)((tprime-h)/h); i++)
+		{
+			P=P+h*SigmaK(omega,t,(h*i))*DA[k][i][(int)(tprime/h)];
+		}
+	DUMMY[(int)((t)/h)][(int)(tprime/h)]=P;
+	P=0.0;
+
+	for (i = (int)((a+h)/h); i <= (int)((t-h)/h); i++)
+		{
+			P=P+h*DR[k][(int)(t/h)][i]*DUMMY[i][(int)(tprime/h)];
+		}
+	IK[(int)((t)/h)][(int)(tprime/h)]=P;
+	P=0.0;
+
+	DK[k][(int)((t)/h)][(int)((tprime)/h)]= DzeroK(omega,t,tprime) + IK[(int)((t)/h)][(int)((tprime)/h)];
+
+}
+}
+
+
+
+
 
 
 
@@ -131,7 +169,7 @@ for (t=tprime+h; t<=b; t=t+h){
 
 
 		for ( t = a+h; t <= b ; t=t+h) {
-			printf("%f\t%f\n",t, DA[(int)((t)/h)][50] );
+			printf("%f\t%f\n",t, DK[k][(int)((t)/h)][25] );
 		}
 
 
