8384c2724d6689a2d51779e75654d934e6d5e76b
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2020-12-29 17:11:01 +0530

    Update green's.h

--- green's.h
+++ green's.h
@@ -6,22 +6,24 @@
 
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
+
+
    /*Function call for DzeroR*/
 
    float DzeroR(float omega, float t, float tprime) {
 
    		float D;
-
    		D = (1/(omega))*sin(omega*(t-tprime));
    		return D;
    	}
+
+
 
    /*Function call for BarDzeroR*/
 
    float BarDzeroR(float omega, float t, float tprime) {
 
    		float D;
-
    		D = cos(omega*(t-tprime));
    		return D;
    	}
@@ -35,19 +37,19 @@
 
      		float D;
 
-     		D = (1/(omega))*sin(omega*(t-tprime));
+        if (t==tprime) {
+        	D = (1/tanh((omega+25)/(2*100)));
+        }
+        if (t>tprime) {
+        	D = (1/omega)*(1/tanh(omega+25/(2*100)))*sin(omega*(t-tprime));
+        }
+        if (t<tprime) {
+          D = -(1/omega)*(1/tanh(omega+25/(2*100)))*sin(omega*(t-tprime));
+        }
+
      		return D;
      	}
 
-   /*Function call for BarDzeroR*/
-
-     float BarDzeroK(float omega, float t, float tprime) {
-
-     		float D;
-
-     		D = cos(omega*(t-tprime));
-     		return D;
-     	}
 
 
     /*Function call for SigmaR*/
@@ -55,33 +57,24 @@
     float SigmaR(float omega, float t, float tprime) {
 
           float D;
-          if (t==tprime) {
-            D = 1;
-          }
-          else {D=0;}
-          return 0;
-          }
-
-    /*Function call for SigmaA*/
-
-    float SigmaA(float omega, float t, float tprime) {
-
-          float D;
-          if (t==tprime) {
-            D = 1;
-          }
-          else {D=0;}
-          return 0;
+          D = (1/(2*10))*Bessel(2*10*(t-tprime))/(t-tprime);
+          return D;
           }
 
     /*Function call for SigmaK*/
 
     float SigmaK(float omega, float t, float tprime) {
 
-          float D;
-          if (t==tprime) {
-            D = 1;
-          }
-          else {D=0;}
-          return 0;
-          }
+
+      float  P, Q, tbath, w;
+      P=0.0;
+      tbath=10;
+
+        for (w = (-2*tbath); w < (2*tbath); w=w+0.1) {
+        Q=(1-(w*w/(4*tbath*tbath)));
+        P=P+(0.1)*(2/tbath)*sqrt(Q)*(sin((w)*(t-tprime)))*tanh((w+25)/(2*25));
+      }
+
+      return P;
+
+      }
