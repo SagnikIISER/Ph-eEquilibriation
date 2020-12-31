bbb630792dda98aa297bc62d7ab0fb31a7b4c5cf
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2020-12-31 11:40:56 +0530

    Update RajdeepEuler.c

--- RajdeepEuler.c
+++ RajdeepEuler.c
@@ -12,7 +12,7 @@ There are two modules written for the Retarded Component
 #include <stdio.h> 						/*Import Standard Module*/
 #include <math.h>							/*Import Math Module*/
 #include <complex.h>          /*Import Modules for Complex Numbers*/
-#include "Bessel.h"
+#include "Dawson.h"
 #include "green's.h"				  /*Import Green's Functions and Baths*/
 
 
@@ -36,7 +36,7 @@ float main() {
 /*Working around the input function call*/
 
 	a= 0.0;										  /*Read value of a*/
-	b= 35.0;										/*Read value of b*/
+	b= 10.0;										/*Read value of b*/
 	h= 0.1;									   	/*Read value of h*/
 
 	lambda=1;									  /*Read value of lambda*/
@@ -60,18 +60,15 @@ float complex DR[n][n];
 float complex BarDR[n][n];
 float complex IR[n][n];
 
-/*
-float complex DA[ktot][n][n];
-float complex BarDA[ktot][n][n];
+
+float complex DA[n][n];
 
 
-float complex DK[ktot][n][n];
-float complex BarDK[ktot][n][n];
-
+float complex DK[n];
 float	complex DUMMY[n][n];
 float complex IK[n][n];
 
-float N[ktot][n]; 							/*Occupation Number at time t and Momentum K*/
+float N[n];
 
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -105,10 +102,10 @@ IR[(int)(t/h)][(int)(t/h)]=0;
 DR[(int)(t/h)][(int)(t/h)]=0;
 BarDR[(int)(t/h)][(int)(t/h)]= 1;
 
-/*
+
 IK[(int)(t/h)][(int)(t/h)]=0;
 DUMMY[(int)(t/h)][(int)(t/h)]=0;
-*/
+
 
 DR[(int)((t+h)/h)][(int)(t/h)]= DzeroR(omega,t+h,t)*I*BarDR[(int)(t/h)][(int)(t/h)];
 IR[(int)((t+h)/h)][(int)(t/h)]=0;
@@ -118,11 +115,14 @@ IR[(int)((t+h)/h)][(int)(t/h)]=0;
 /*The Retarded Part*/
 
 
-float complex P; P=0.0;
+float complex P;
+P=0.0;
+
 int i;
 
-for (t=tprime+h; t<=b; t=t+h)
-	for (tprime=a; tprime<=b; tprime=tprime+h){
+
+	for (tprime=a; tprime<b; tprime=tprime+h){
+	for (t=tprime+h; t<b; t=t+h){
 	 {BarDR[(int)(t/h)][(int)(tprime/h)]=  BarDzeroR(omega,t,t-h)*BarDR[(int)((t-h)/h)][(int)(tprime/h)]-omega*omega*DzeroR(omega,t,t-h)*DR[(int)((t-h)/h)][(int)(tprime/h)]+(h/2)*BarDzeroR(omega,t,t-h)*IR[(int)((t-h)/h)][(int)(tprime/h)];
 		DR[(int)((t+h)/h)][(int)(tprime/h)]= DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(tprime/h)]+BarDzeroR(omega,t+h,t)*DR[(int)(t/h)][(int)(tprime/h)]+(h/2)*DzeroR(omega,t+h,t)*IR[(int)(t/h)][(int)(tprime/h)];
 
@@ -135,42 +135,46 @@ for (t=tprime+h; t<=b; t=t+h)
 		P=0.0;
 	}
 	}
+}
 
 /*The Advanced Part*/
-/*
+
 for (tprime=a; tprime<=b; tprime=tprime+h){
 for (t=tprime+h; t<=b; t=t+h){
-		DA[k][(int)((tprime)/h)][(int)(t/h)]=DR[(int)((t)/h)][(int)(tprime/h)];
-		BarDA[k][(int)((tprime)/h)][(int)(t/h)]=BarDR[(int)((t)/h)][(int)(tprime/h)];
+		DA[(int)((tprime)/h)][(int)(t/h)]=conjf(DR[(int)((t)/h)][(int)(tprime/h)]);
 }
 }
 
 /*The Keldysh Part*/
 
-/*
+P=0.0;
 
-for ( t = a; t <=b ; t=t+h) {
-for ( tprime = a; tprime <=b ; tprime=tprime+h) {
+for ( ttwo = a; ttwo <=(b-h) ; ttwo=ttwo+h) {
+for ( tone = a; tone <=(b-h) ; tone=tone+h) {
 
-	for (i = (int)((a+h)/h); i <= (int)((t-h)/h); i++)
+	for (i = 0; i <= (int)((ttwo-h)/h); i++)
 		{
-			P=P+h*SigmaK(omega,t,(h*i))*DA[k][i][(int)(tprime/h)];
+			P=P+h*SigmaK(omega,tone,(h*i))*DA[i][(int)(ttwo/h)];
 		}
-	DUMMY[(int)((t)/h)][(int)(tprime/h)]=P;
+	DUMMY[(int)((tone)/h)][(int)((ttwo)/h)]=P;
 	P=0.0;
+		}
+	}
 
-	for (i = (int)((a+h)/h); i <= (int)((t-h)/h); i++)
+for ( ttwo = a; ttwo <=(b-h) ; ttwo=ttwo+h) {
+for ( tone = a; tone <=(b-h) ; tone=tone+h) {
+
+	for (i = (int)((a+h)/h); i <= (int)((tone-h)/h); i++)
 		{
-			P=P+h*DR[(int)(t/h)][i]*DUMMY[i][(int)(tprime/h)];
+			P=P+h*DR[(int)(tone/h)][i]*DUMMY[i][(int)(ttwo/h)];
 		}
-	IK[(int)((t)/h)][(int)(tprime/h)]=P;
+	IK[(int)((tone)/h)][(int)((ttwo)/h)]=P;
 	P=0.0;
-
-
-	DK[k][(int)((t)/h)][(int)((tprime)/h)]= DzeroK(omega,t,tprime)+IK[(int)((t)/h)][(int)(tprime/h)];
-
-}
-}
+		}
+		}
+for ( t = a; t <=(b-h) ; t=t+h) {
+DK[(int)((t)/h)]= DzeroK(omega,t,t)+IK[(int)((t)/h)][(int)((t)/h)];
+		}
 
 
 /*The Occupation Number*/
@@ -183,7 +187,7 @@ for ( tprime = a; tprime <=b ; tprime=tprime+h) {
 
 /*The Statistics*/
 /*
-float VarE[n], EE[n], E[n], Ntot[n];
+float VarE[n], EE[n], E[n], Ntot[n];-2*omega*Dawson(omega/(sqrt(2)*sigma))/sqrt(3.14159265)
 float EEPr, EPr, NPr;
 
 EPr=0.0;
@@ -193,7 +197,7 @@ NPr=0.0;
 , cimagf(DR[0][(int)(t/h)][0])
 
 
-		for ( t = a; t <=b ; t=t+h) {
+		for ( t = a; t <=b ; t=t+h) {-2*omega*Dawson(omega/(sqrt(2)*sigma))/sqrt(3.14159265)
 		for ( k = 0; k <ktot ; k=k+1) {
 					NPr=NPr+N[k][(int)((t)/h)];
 				}
@@ -237,7 +241,7 @@ K loop ends here.
 Put K independent printf statements after this.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-for (t=a; t<=b; t=t+h){
-printf("%f\t%f\n", t ,cimagf(DR[(int)(t/h)][0]) );
+for (t = a; t < b; t=t+h){
+printf("%f\t%f\n", t ,crealf(DK[(int)(t/h)]) ) ;
 }
 }
