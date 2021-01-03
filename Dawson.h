89344dbae0f2a4332d4373731e82fe1cc7204094
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2021-01-03 23:52:02 +0530

    Module for computing Dawson Fn

--- Dawson.h
+++ Dawson.h
@@ -1,18 +1,52 @@
+
+
+
 float Dawson( float x )
-  /*------------------------------------------------------------*/
-  /* PURPOSE: Evaluate Bessel function of first kind and order  */
-  /*          1 at input x                                      */
-  /*------------------------------------------------------------*/
+
   {
      float h , P;
-     int i;
+     float a;
      h=0.1;
-     P=0.00;
-
-     for (i=0; i < (x/h); i++) {
-        P=P+h*exp(i*i*h*h);
+     P=0.0;
+     for (a=h; a < fabs(x); a=a+h) {
+        P=P+h*exp((a*a));
      }
-     P=P+h+(h*2)*exp(x*x);
-     P=P*exp(-x*x);
-     return P;
+
+     if (x>0) {
+        P=P+(h/2)+(h/2)*(exp(x*x));
+        P=P*exp(-x*x);
+     }
+
+      if (x<0) {
+        P=P+(h/2)-(h/2)*(exp(x*x));
+        P=P*exp(-x*x);
+      }
+
+
+      return P;
+
+
   }
+
+/*
+  double dawsonf(float x){
+          int j,N;
+          double r,y,delx=pow(10.0,-4);
+          double complex *term;
+          double sum=0.0;
+
+          N=(int)(fabs(x/delx))+1;
+          term=(double complex *)malloc(N*sizeof(double complex));
+
+          for(j=0;j<N;j++){
+                  y=j*delx;
+                  r=y*y-x*x;
+                  term[j]=exp(r)+I*0.0;
+          }// j loop ends
+          sum=creal(simpson_comb(term,N,delx));
+          if(x<0.0) sum*=-1.0;
+
+          free(term);
+          return sum;
+        }
+*/
