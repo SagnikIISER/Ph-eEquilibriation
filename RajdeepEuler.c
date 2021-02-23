89fefd0e127f3afe9a9181b229afa9a44fe6c6f6
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2021-02-24 00:43:57 +0530

    Update RajdeepEuler.c

--- RajdeepEuler.c
+++ RajdeepEuler.c
@@ -21,35 +21,40 @@ There are two modules written for the Retarded and Keldysh Component:
 
 float main() {
 
+
 /*Definitions*/
 
-	float t, tprime;		/*time parameters*/
-  	float a,b; 			/*to be used for lower and upper bound on time*/
-  	float tone, ttwo, tthree;	/*extra variables*/
-  	float h;			/*gap in time, "the epsilon"*/
+float t, tprime;			/*time parameters*/
+float a,b; 				/*to be used for lower and upper bound on time*/
+float tone, ttwo;			/*extra variables*/
+float h;				/*gap in time, "the epsilon"*/
 
-	float A;			/*Lattice Constant*/
-	int   n, k, ktot;		/*array dimension*/
-	float K;			/*Momentum formula*/
-  	float omega;			/*omega, k; materials for dispersion relation*/
-	float lambda;			/*perturbation strenght parameter*/
+float A;				/*Lattice Constant*/
+int   n, k, ktot;			/*Array Dimension*/
+float K;				/*Momentum Formula*/
+float omega;				/*Initial Energy Level*/
+float lambda;				/*Perturbation Strength*/
+
+float sigma, Tbath, Tsyst;
+
 
 
 /*Working around the input function call*/
 
-	a= 0.0;				/*Read value of a*/
-	b= 30.0;			/*Read value of b*/
-	h= 0.1;				/*Read value of h*/
+	a= 0.0;					/*Read value of a*/
+	b= 30.0;				/*Read value of b*/
+	h= 0.1;					/*Read value of h*/
 
-	lambda=1;			/*Read value of lambda*/
+	lambda=1.0;				/*Read value of lambda*/
 
- 	n=(b-a)/h+1;    		/*array Dimension*/
-	ktot=25;			/*array Dimension*/
+ 	n=(b-a)/h+1;    			/*array Dimension*/
+	ktot= 35;				/*array Dimension*/
 
-	A=1;				/*Read value of Lattice Constant*/
-        k=5     			/*Dummy Array for momentum*/
+	A=1;					/*Read value of Lattice Constant*/		
+	k=5;	    				/*Dummy Array for momentum*/
 
-  	tprime = a;
+	sigma=5.0;
+	Tsyst=1.0;
 
 
 
@@ -62,18 +67,15 @@ float complex DR[n][n];
 float complex BarDR[n][n];
 float complex IR[n][n];
 
-
 float complex DA[n][n];
 
-
 float complex DK[n][n];
-float	complex DUMMY1[n][n];
-float	complex DUMMY2[n][n];
-float	complex DUMMY3[n][n];
-float complex IK1[n][n];
-float complex IK2[n][n];
-float complex IK3[n][n];
+float complex BarDK[n][n];
+float complex DKthermal;
+float complex DKthermal2;
 
+float	complex IK1[n][n];
+float	complex IK2[n][n];
 
 float N[n];
 
@@ -89,13 +91,14 @@ The Module for Dyson Iteration using Euler Method:
 Momentum values at Dummy Indicies
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-K=((-3.1418/A)+k*(6.2836/A)/(ktot-1));
+K=((-3.1418/A)+k*(6.2836/A)/(ktot-1.0));
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 System Dispersion Relation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-omega=2*sqrt(sin((K*A/2))*sin((K*A/2)));
+omega=2.0*sqrt(sin((K*A/2.0))*sin((K*A/2.0)));
+
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  To Plot the Dispersion Relation
@@ -104,62 +107,90 @@ omega=2*sqrt(sin((K*A/2))*sin((K*A/2)));
 //	printf("%f\t%f\n", K, omega );
 
 
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Computing the DK thermal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 
-/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+float akka;
+float complex P;
+
+/*
+for (Tbath=0.1 ; Tbath <= 4 ;  Tbath= Tbath+h) {
+
+P=0.0;
+
+for(akka=-100; akka <= 100; akka=akka+h){
+P = P + h*(-lambda*lambda*akka*exp(-(akka*akka/(sigma*sigma))))/((2.0*akka*akka-2.0*omega*omega-lambda*lambda*(2.0/sqrt(3.14159265))*akka*Dawson(akka/sigma))*(2.0*akka*akka-2.0*omega*omega-lambda*lambda*(2.0/sqrt(3.14159265))*akka*Dawson(akka/sigma))+(lambda*lambda*lambda*lambda*akka*akka*exp(-2.0*(akka*akka/(sigma*sigma)))))*(1.0/tanh((akka/(2.0*Tbath))));
+}
+DKthermal = P/(3.14159265);
+
+
+
+
+printf("%f\t%f\n", Tbath, -crealf(DKthermal));
+
+
+}
+
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 /*Initial Condition*/
-
 
 for (t=a; t<=b; t=t+h){
 
 
 IR[(int)(t/h)][(int)(t/h)]=0;
 DR[(int)(t/h)][(int)(t/h)]=0;
-BarDR[(int)(t/h)][(int)(t/h)]= 1;
+
+BarDR[(int)(t/h)][(int)(t/h)]= 1/2.0;
+
+DK[(int)(t/h)][(int)(t/h)]=-(I/(2.0*omega))*(1.0/(tanh((omega)/(2*Tsyst))));
+BarDK[(int)(t/h)][(int)(t/h)]= 0;
+
+DR[(int)((t+h)/h)][(int)(t/h)]= 2.0*DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(t/h)];
+//IR[(int)((t+h)/h)][(int)(t/h)]= -(1.0/(2.0*sqrt(3.14159265)))*lambda*lambda*sigma*DR[(int)((t+h)/h)][(int)(t/h)];
+
+
+DK[(int)((t+h)/h)][(int)(t/h)]= 2.0*BarDzeroR(omega,t+h,t)*DK[(int)(t/h)][(int)(t/h)];
+DK[(int)((t)/h)][(int)((t+h)/h)]=-conjf(DK[(int)((t+h)/h)][(int)(t/h)]);
 
 
 IK1[(int)(t/h)][(int)(t/h)]=0;
 IK2[(int)(t/h)][(int)(t/h)]=0;
-IK3[(int)(t/h)][(int)(t/h)]=0;
-DUMMY1[(int)(t/h)][(int)(t/h)]=0;
-DUMMY2[(int)(t/h)][(int)(t/h)]=0;
-DUMMY3[(int)(t/h)][(int)(t/h)]=0;
-
-
-DR[(int)((t+h)/h)][(int)(t/h)]= DzeroR(omega,t+h,t)*I*BarDR[(int)(t/h)][(int)(t/h)];
-IR[(int)((t+h)/h)][(int)(t/h)]=0;
-IK1[(int)((t+h)/h)][(int)(t/h)]=0;
-IK2[(int)((t+h)/h)][(int)(t/h)]=0;
-IK3[(int)((t+h)/h)][(int)(t/h)]=0;
-
+//IK1[(int)((t+h)/h)][(int)(t/h)]=(h/2.0)*lambda*lambda*SigmaR(t+h,t)*DK[(int)(t/h)][(int)(t/h)]-(1.0/(2.0*sqrt(3.14159265)))*lambda*lambda*sigma*DK[(int)((t+h)/h)][(int)(t/h)];
+//IK2[(int)((t+h)/h)][(int)(t/h)]=(h/2.0)*lambda*lambda*SigmaK(t+h,t+h)*DR[(int)((t)/h)][(int)(t+h/h)];
 }
 
-/*The Retarded Part*/
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+The Retarded Part
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-
-float complex P;
 P=0.0;
 int i;
 
 
-	for (tprime=a; tprime<b; tprime=tprime+h){
 	for (t=tprime+h; t<b; t=t+h){
-	 {BarDR[(int)(t/h)][(int)(tprime/h)]=  BarDzeroR(omega,t,t-h)*BarDR[(int)((t-h)/h)][(int)(tprime/h)]-omega*omega*DzeroR(omega,t,t-h)*DR[(int)((t-h)/h)][(int)(tprime/h)]+(h/2)*BarDzeroR(omega,t,t-h)*IR[(int)((t-h)/h)][(int)(tprime/h)];
-		DR[(int)((t+h)/h)][(int)(tprime/h)]= DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(tprime/h)]+BarDzeroR(omega,t+h,t)*DR[(int)(t/h)][(int)(tprime/h)]+(h/2)*DzeroR(omega,t+h,t)*IR[(int)(t/h)][(int)(tprime/h)];
+	 {BarDR[(int)(t/h)][(int)(tprime/h)] =2.0*BarDzeroR(omega,t,t-h)*BarDR[(int)((t-h)/h)][(int)(tprime/h)]-2.0*omega*omega*DzeroR(omega,t,t-h)*DR[(int)((t-h)/h)][(int)(tprime/h)]+(h/2.0)*BarDzeroR(omega,t,t-h)*IR[(int)((t-h)/h)][(int)(tprime/h)];
+	 for (tprime=a; tprime<b; tprime=tprime+h){
+		DR[(int)((t+h)/h)][(int)(tprime/h)]=2.0*DzeroR(omega,t+h,t)*BarDR[(int)(t/h)][(int)(tprime/h)]+2.0*BarDzeroR(omega,t+h,t)*DR[(int)(t/h)][(int)(tprime/h)]+(h/2.0)*DzeroR(omega,t+h,t)*IR[(int)(t/h)][(int)(tprime/h)];
 
 
 				for (i = (int)((tprime+h)/h); i <= (int)((t)/h); i++)
 				{
 					P=P+h*lambda*lambda*SigmaR(t+h,(h*i))*DR[i][(int)(tprime/h)];
 				}
-		IR[(int)((t+h)/h)][(int)(tprime/h)]=P;
+
+		IR[(int)((t+h)/h)][(int)(tprime/h)]=P-(1.0/(2.0*sqrt(3.14159265)))*lambda*lambda*sigma*DR[(int)((t+h)/h)][(int)(tprime/h)];
 		P=0.0;
 	}
 	}
-}
+	}
 
-/*The Advanced Part*/
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+The Advanced Part
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 for (tprime=a; tprime<=b; tprime=tprime+h){
 for (t=a; t<=b; t=t+h){
@@ -173,90 +204,35 @@ for (t=a; t<=b; t=t+h){
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 
-/*The convolution RKA*/
+	for (tprime=a; tprime<b; tprime=tprime+h){
+	for (t=a+h; t<b; t=t+h){
+	 {BarDK[(int)(t/h)][(int)(tprime/h)] = 2.0*BarDzeroR(omega,t,t-h)*BarDK[(int)((t-h)/h)][(int)(tprime/h)]-2.0*omega*omega*DzeroR(omega,t,t-h)*DK[(int)((t-h)/h)][(int)(tprime/h)]+(h/2.0)*BarDzeroR(omega,t,t-h)*(IK1[(int)((t-h)/h)][(int)(tprime/h)]+IK2[(int)((t-h)/h)][(int)(tprime/h)]);
+		DK[(int)((t+h)/h)][(int)(tprime/h)]= 2.0*DzeroR(omega,t+h,t)*BarDK[(int)(t/h)][(int)(tprime/h)]+2.0*BarDzeroR(omega,t+h,t)*DK[(int)(t/h)][(int)(tprime/h)]+(h/2.0)*DzeroR(omega,t+h,t)*(IK1[(int)(t/h)][(int)(tprime/h)]+IK2[(int)(t/h)][(int)(tprime/h)]);
 
 
-P=0.0;
+				for (i = (int)((a+h)/h); i <= (int)((t)/h); i++)
+				{
+					P=P+h*lambda*lambda*SigmaR(t+h,(h*i))*DK[i][(int)(tprime/h)];
+				}
 
-for ( ttwo = a; ttwo <=b ; ttwo=ttwo+h) {
-for ( tone = a; tone <=b ; tone=tone+h) {
+		IK1[(int)((t+h)/h)][(int)(tprime/h)]=P+(h/2.0)*lambda*lambda*SigmaR(t+h,tprime)*DK[(int)(tprime/h)][(int)(tprime/h)]-(1.0/(2.0*sqrt(3.14159265)))*lambda*lambda*sigma*DK[(int)((t+h)/h)][(int)(tprime/h)];
+		P=0.0;
 
-	for (i = (int)((a+h)/h); i <= (int)((ttwo-h)/h); i++)
-		{
-			P=P+h*lambda*lambda*SigmaK(tone,(h*i))*DA[i][(int)(ttwo/h)];
-		}
-	DUMMY1[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-	P=0.0;
-		}
+
+						for (i = (int)((a+h)/h); i < (int)((tprime)/h); i++)
+						{
+							P=P+h*lambda*lambda*SigmaK(t+h,(h*i))*DA[i][(int)(tprime/h)];
+						}
+
+		IK2[(int)((t+h)/h)][(int)(tprime/h)]=P+(h/2.0)*lambda*lambda*SigmaK(t+h,t+h)*DA[(int)((t+h)/h)][(int)(tprime/h)];
+		P=0.0;
+
+		DK[(int)((tprime)/h)][(int)(t/h)]=-conjf(DK[(int)(t/h)][(int)(tprime/h)]);
+		BarDK[(int)((tprime)/h)][(int)(t/h)]=conjf(BarDK[(int)(t/h)][(int)(tprime/h)]);
+
 	}
-
-for ( ttwo = a; ttwo <=b ; ttwo=ttwo+h) {
-for ( tone = a; tone <=b ; tone=tone+h) {
-
-	for (i = (int)((a+h)/h); i <= (int)((tone-h)/h); i++)
-		{
-			P=P+h*DzeroR(omega, tone, (h*i))*DUMMY1[i][(int)(ttwo/h)];
-		}
-	IK1[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-	P=0.0;
-		}
-		}
-
-
-/* The convolution KAA*/
-
-	P=0.0;
-
-	for ( ttwo = a; ttwo <=b ; ttwo=ttwo+h) {
-	for ( tone = a; tone <=b ; tone=tone+h) {
-
-	for (i = (int)((tone+h)/h); i <= (int)((ttwo-h)/h); i++)
-		{
-			P=P+h*lambda*lambda*SigmaA(tone,(h*i))*DA[i][(int)(ttwo/h)];
-		}
-	DUMMY2[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-	P=0.0;
-		}
-		}
-
-	for ( ttwo = a; ttwo <=b ; ttwo=ttwo+h) {
-	for ( tone = a; tone <=b ; tone=tone+h) {
-
-	for (i = (int)((a+h)/h); i <= (int)((ttwo-h)/h); i++)
-		{
-			P=P+h*DzeroK(omega,tone,(h*i))*DUMMY2[i][(int)(ttwo/h)];
-		}
-	IK2[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-	P=0.0;
-		}
-		}
-
-
-/*The convolution RRK
-  and the updated Keldysh Component*/
-
-	for ( ttwo = a; ttwo <=b ; ttwo=ttwo+h) {
-	for ( tone = a; tone <=b ; tone=tone+h) {
-
-		for (i = (int)((a+h)/h); i <= (int)((tone-h)/h); i++)
-			{
-			P=P+h*lambda*lambda*SigmaR(tone,(h*i))*DK[i][(int)(ttwo/h)];
-			}
-			DUMMY3[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-			P=0.0;
-
-		for (i = (int)((a+h)/h); i <= (int)((tone-h)/h); i++)
-			{
-			P=P+h*DzeroR(omega,tone,(h*i))*DUMMY3[i][(int)(ttwo/h)];
-			}
-
-		  IK3[(int)((tone)/h)][(int)((ttwo)/h)]=P;
-			P=0.0;
-
-			DK[(int)((tone)/h)][(int)((ttwo)/h)]=DzeroK(omega,tone,ttwo)+IK1[(int)((tone)/h)][(int)((ttwo)/h)]+IK2[(int)((tone)/h)][(int)((ttwo)/h)]+IK3[(int)((tone)/h)][(int)((ttwo)/h)];
-		}
-		}
-
+	}
+	}
 
 
 
@@ -316,17 +292,18 @@ NPr=0.0;
 
 //}
 
+*/
 /*	for (t=a; t<=b; t=t+h){
 			 printf("%f\t%f\n", t, DK[0][(int)((t)/h)][(int)((t)/h)] );
 		 }
-*/
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 K loop ends here.
 Put K independent printf statements after this.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+//printf("\n%f\t%f", omega, crealf(DKthermal))  ;
 
 for (t = a; t < (b-h); t=t+h){
-printf("%f\t%f\n", t , -(2*omega)*cimagf(DK[(int)((t)/h)][(int)((t)/h)])) ;
+printf("%f\t%f\n", t ,  -cimagf(DK[(int)(t/h)][(int)(t/h)]))  ;
 }
 }
