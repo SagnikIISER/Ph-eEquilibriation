2b815af9f89054c46c61a4ae31fd796a55ffaf5b
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2021-02-25 21:44:01 +0530

    Update green's.h

--- green's.h
+++ green's.h
@@ -10,13 +10,16 @@
 
    /*Function call for DzeroR*/
 
-    float complex DzeroR(float omega, float t, float tprime) {
+    float complex DzeroR (float omega, float t, float tprime)
+        {
         float complex D;
-        if (t<tprime) {
+        if (t<tprime)
+        {
             D=0;
         }
-   		  else{
-          D = (1/omega)*sin(omega*(t-tprime));
+
+        else{
+          D = -(1/(2.0*omega))*sin(omega*(t-tprime));
         }
 
    		  return D;
@@ -25,106 +28,93 @@
 
    /*Function call for BarDzeroR*/
 
-     float complex BarDzeroR(float omega, float t, float tprime) {
-
-       float complex D;
-       if (t<tprime) {
+     float complex BarDzeroR(float omega, float t, float tprime)
+        {
+        float complex D;
+        if (t<tprime)
+        {
            D=0;
-       }
-       else{
-           D = cos(omega*(t-tprime));
-       }
+        }
+        else{
+           D = -cos(omega*(t-tprime))/2.0;
+         }
 
        return D;
        }
 
 
-   /*Function call for DzeroR*/
-
-        float complex DzeroA(float omega, float t, float tprime) {
-            float complex D;
-            if (t>tprime) {
-                D=0;
-            }
-       		  else{
-              D = (1/omega)*sin(omega*(tprime-t));
-            }
-
-       		  return D;
-       	}
-
-
     /*Function call for DzeroK*/
 
-     float complex DzeroK(float omega, float t, float tprime) {
+     float complex DzeroK(float omega, float Tsyst, float t, float tprime) {
 
      		float complex D;
-        float Tsyst;
-        Tsyst= 0.5;
-        	D = -(I/(2*omega))*((cos((-omega*(t-tprime)))/(tanh((omega)/(2*Tsyst)))));//-I*(1/(2*omega))*(1+exp(-(omega/Tsyst))*(cos(omega*(t-tprime))+I*sin(omega*(t-tprime))))
+        	D = -(I/(2.0*omega))*((cos((omega*(t-tprime)))/(tanh(omega/(2.0*Tsyst)))));
         	return D;
      	}
 
 
 
+     /*Function call for DzeroK*/
+
+      float complex BarDzeroK(float omega, float Tsyst, float t, float tprime) {
+
+          float complex D;
+          D = (I/2.0)*((sin((omega*(t-tprime)))/(tanh((omega)/(2*Tsyst)))));
+          return D;
+      }
+
+
+
     /*Function call for SigmaR*/
 
-     float complex SigmaR( float t, float tprime) {
+     float complex SigmaR( float t, float tprime)
+     {
 
-
-              float complex D,P;
+              float complex D;
               float sigma;
-              float akka;
-              float h;
 
-              h=0.1;
-              P=0.0;
-              sigma = 1.0;
+              sigma = 5.0;
 
               if (t<tprime) {
                   D=0;
               }
          		  else{
-                /*for(akka=-5; akka <= 5; akka=akka+h){
-                P = P+ h*(1/(2*3.14159265))*(2*akka*Dawson(akka/(sqrt(2)*sigma))/sqrt(3.14159265)+I*akka*exp(-(akka*akka)/(sigma*sigma)))*(cos(akka*(t-tprime))+I*sin(akka*(t-tprime)));
-                }
-                D = P;
-                P = 0.0;*/
-                D= 2*sigma*sigma*sigma*(t-tprime)*exp(-sigma*sigma*(t-tprime)*(t-tprime)/2);
+                D= -(1.0/(2.0*sqrt(3.14159265)))*sigma*sigma*sigma*(t-tprime)*exp(-sigma*sigma*(t-tprime)*(t-tprime)/4.0);
               }
 
          		  return D;
          	}
 
 
-          /*Function call for SigmaA*/
+      /*Function call for SigmaPR*/
+/*
+               float complex SigmaPR( float t, float tprime) {
 
-           float complex SigmaA( float t, float tprime) {
 
-                           float complex D,P;
-                           float sigma;
-                           float akka;
-                           float h;
+                        float complex D,P;
+                        float sigma;
+                        float akka;
+                        float h;
 
-                           h=0.1;
-                           P=0.0;
-                           sigma = 1.0;
+                        h=0.1;
+                        P=0.0;
+                        sigma = 5.0;
 
-                    if (t>tprime) {
-                        D=0;
-                    }
-               		  else{
-/*                      for(akka=-5; akka <= 5; akka=akka+h){
-                      P = P+ h*(1/(2*3.14159265))*(2*akka*Dawson(akka/(sqrt(2)*sigma))/sqrt(3.14159265)+I*akka*exp(-(akka*akka)/(sigma*sigma)))*(cos(akka*(t-tprime))-I*sin(akka*(t-tprime)));
-                      }
-                      D = P;
-                      P = 0.0;
+                        if (t<tprime) {
+                            D=0;
+                        }
+                   		  else{
+                          /*for(akka=-5; akka <= 5; akka=akka+h){
+                          P = P+ h*(1/(2*3.14159265))*(2*akka*Dawson(akka/(sqrt(2)*sigma))/sqrt(3.14159265)+I*akka*exp(-(akka*akka)/(sigma*sigma)))*(cos(akka*(t-tprime))+I*sin(akka*(t-tprime)));
+                          }
+                          D = P;
+                          P = 0.0;
+                          D= (1/(2.0*sqrt(3.14159265)))*sigma*sigma*sigma*(1.0-2.0*sigma*sigma*(t-tprime)*(t-tprime))*exp(-sigma*sigma*(t-tprime)*(t-tprime)/4.0);
+                        }
+
+                   		  return D;
+                   	}
 */
-                      D= 2*sigma*sigma*sigma*(t-tprime)*exp(-sigma*sigma*(t-tprime)*(t-tprime)/2);;
-                    }
-
-               		  return D;
-               	}
 
     /*Function call for SigmaK*/
 
@@ -138,13 +128,13 @@
 
                                  h=0.1;
                                  P=0.0;
-                                 sigma = 1.0;
-                                 Tbath = 0.9;
+                                 sigma = 5.0;
+                                 Tbath = 1.0;
 
-                                 for(akka=-5; akka <= 5; akka=akka+h){
-                                 P = P+ h*(-2*I*akka*exp(-(akka*akka)/(sigma*sigma)))*(1/tanh((akka/(2*Tbath))))*(cos(akka*(t-tprime))-I*sin(akka*(t-tprime)));
+                                 for(akka=-40.0; akka <= 40.0; akka=akka+h){
+                                 P = P+ h*(-2.0*I*akka*exp(-(akka*akka)/(sigma*sigma)))*(1.0/tanh((akka/(2.0*Tbath))))*((cos(akka*(t-tprime)))-I*sin(akka*(t-tprime)));
                                  }
-                                 D =(P/(2*3.14159265));
+                                 D = P/(2.0*3.14159265);
                                  P = 0.0;
 
              return D;
