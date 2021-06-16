21666bc0017f19adf942e99f58665fe77bfc8a95
Sagnik Ghosh <71169972+SagnikIISER@users.noreply.github.com>
2021-06-16 16:25:00 +0530

    Add files via upload

--- FermiTest.c
+++ FermiTest.c
@@ -1,4 +1,3 @@
-
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 CODE for SOLVING DYSON EQUATION
 
@@ -25,14 +24,13 @@ DR, BarDR and the integration I
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 double complex GR[2510][2510];
-double complex BarGR[2510][2510];
 
 double complex SigK[2510][2510];
 double complex GK[2510][2510];
-double complex BarGK[2510][2510];
-double complex DKthermal;
 
-double complex I4, I5;
+double complex I2, I3, I4, I5;
+
+
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Main Module
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
@@ -42,40 +40,36 @@ double main() {
 
 /*Definitions*/
 
-double t, tprime;				/*time parameters*/
-double a,b; 					/*to be used for lower and upper bound on time*/
-double tone, ttwo;				/*extra variables*/
-double h;					/*gap in time, "the epsilon"*/
+double a,b; 					    /*to be used for lower and upper bound on time*/
+double h;					        /*gap in time, "the epsilon"*/
 
-double A;					/*Lattice Constant*/
-int    n, klevel, ktot;				/*Array Dimension*/
-double K;					/*Momentum Formula*/
-double epsilon;					/*Initial Energy Level*/
-double lambda;					/*Perturbation Strength*/
-double nomega;          /*Density of states*/
+double A;				         	/*Lattice Constant*/
+int    n, klevel, ktot;		/*Array Dimension*/
+double K;					        /*Momentum Formula*/
+double epsilon;					  /*Initial Energy Level*/
+double lambda;					  /*Perturbation Strength*/
+double nomega;            /*Density of states*/
 
-double Tbath, Telectron, nu;
+double Telectron, nu;
 
-
-double akka;
 double complex P;
 
 
 /*Working around the input variable calls*/
 
   a= 0.000000;					/*Read value of a*/
-  b= 50.000000;				 /*Read value of b*/
+  b= 40.000000;				  /*Read value of b*/
   h= 0.050000;					/*Read value of h*/
 
   lambda=1.00000;				/*Read value of lambda*/
-  n=(int)((b-a)/h)+1;			    	/*array Dimension*/
-  ktot= 19;					/*array Dimension*/
+  n=(int)((b-a)/h)+1;		/*array Dimension*/
+  ktot= 19;					    /*array Dimension*/
 
-  A= 1.0;						/*Read value of Lattice Constant*/
-  klevel= 0; 			    		/*Dummy Array for momentum*/
+  A= 1.0;					    	/*Read value of Lattice Constant*/
+  klevel= 0; 			    	/*Dummy Array for momentum*/
 
   Telectron=0.000000;
-  nu=1.0;
+  nu=0.5;
 
 int i;
 int j;
@@ -99,7 +93,7 @@ Momentum values at Dummy Indicies
 System Dispersion Relation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
- epsilon = K*K/2;
+ epsilon = 0.0;
  //nomega= 1/(exp(omega/Tsyst)-1);
 
 
@@ -141,19 +135,9 @@ Initial Condition
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
 
-
-for (i=0; i<n; i++){
-
-
-            GR[i][i]= I;
-            GK[i][i]= -I*tanh((epsilon-nu)/(2.0*Telectron));
-
-            GR[i+1][i]= GzeroR(epsilon,(i*h)+h,(i*h));
-
-            GK[i+1][i]= GzeroK(epsilon,Telectron,nu,(i*h)+h,(i*h));
-            GK[i][i+1]= -conjf(GK[i+1][i]);
-
-}
+GK[0][0]= -I*tanh((epsilon-nu)/(2.0*Telectron));
+GK[1][0]= GzeroK(epsilon,Telectron,nu, h , 0);
+GK[0][1]= -conjf(GK[1][0]);
 
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -163,15 +147,28 @@ The Retarded Part
 
 for (i=0; i<n; i++){
     	for (j=1; j<i; j++){
+
+
         for (l = j+1; l < i; l++)
         {
           P=P+h*lambda*lambda*SigmaR((i*h),(h*l))*GR[l][j];
         }
 
-        I4=P;
+        I4=P+(h/2.0)*SigmaR((i*h),(h*i))*GR[i][j]+(h/2.0)*SigmaR((i*h),(h*j))*GR[j][j];
         P=0.0;
 
-        GR[i+1][j]= I*GzeroR(epsilon,(i*h)+h,(i*h))*GR[i][j]+(h/2.0)*GzeroR(epsilon,(i*h)+h,(i*h))*I4;
+
+
+        for (l = j+1; l < i+1; l++)
+        {
+          P=P+h*lambda*lambda*SigmaR((i*h)+h,(h*l))*GR[l][j];
+        }
+
+        I2=P+(h/2.0)*SigmaR((i*h)+h,(h*j))*GR[j][j];
+        P=0.0;
+
+
+        GR[i+1][j]= (I*GzeroR(epsilon,(i*h)+h,(i*h))*GR[i][j]+(h/2.0)*GzeroR(epsilon,(i*h)+h,(i*h))*I4-I*(h/2.0)*I2)/(1+I*(h/2.0)*SigmaR((i*h)+h,(h*i)+h));
 
     	}
 
@@ -181,12 +178,14 @@ for (i=0; i<n; i++){
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
     for (j=0; j<i; j++){
+
+
     for (l = 1; l <= i; l++)
       {
         P=P+h*lambda*lambda*SigmaR((i*h),(h*l))*GK[l][j];
       }
 
-      I4=P+(h/2.0)*lambda*lambda*SigmaR((i*h),(j*h))*GK[j][j];;
+      I4=P+(h/2.0)*lambda*lambda*SigmaR((i*h),(i*h))*GK[i][j]+(h/2.0)*lambda*lambda*SigmaR((i*h),(j*h))*GK[j][j];
       P=0.0;
 
           for (l = 1; l < j; l++)
@@ -194,34 +193,77 @@ for (i=0; i<n; i++){
             P = P + h*lambda*lambda*SigK[i][l]*conjf(GR[j][l]);
           }
 
-      I5= P + (h/2.0)*lambda*lambda*SigK[i][i]*conjf(GR[j][i]);
+      I5= P + (h/2.0)*lambda*lambda*SigK[i][j]*conjf(GR[j][j])+ (h/2.0)*lambda*lambda*SigK[i][i]*conjf(GR[j][i]);
       P=0;
 
-      GK[i+1][j]= I*GzeroR(epsilon,(i*h)+h,(i*h))*GK[i][j]+(h/2.0)*GzeroR(epsilon,(i*h)+h,(i*h))*(I4+I5);
+
+
+
+      for (l = 1; l <= i; l++)
+        {
+          P=P+h*lambda*lambda*SigmaR((i*h)+h,(h*l))*GK[l][j];
+        }
+
+        I2=P+(h/2.0)*lambda*lambda*SigmaR((i*h)+h,(j*h))*GK[j][j];
+        P=0.0;
+
+            for (l = 1; l < j; l++)
+            {
+              P = P + h*lambda*lambda*SigK[i+1][l]*conjf(GR[j][l]);
+            }
+
+        I3= P + (h/2.0)*lambda*lambda*SigK[i+1][j]*conjf(GR[j][j])+ (h/2.0)*lambda*lambda*SigK[i+1][i+1]*conjf(GR[j][i+1]);
+        P=0;
+
+
+
+      GK[i+1][j]= (I*GzeroR(epsilon,(i*h)+h,(i*h))*GK[i][j]+(h/2.0)*GzeroR(epsilon,(i*h)+h,(i*h))*(I4+I5)-(h/2.0)*I*(I2+I3))/(1+I*(h/2.0)*lambda*lambda*SigmaR((i*h)+h,(i*h)+h));
 		  GK[j][i+1]=-conjf(GK[i+1][j]);
 		}
 
   for (k=0; k<=i; k++)
-
   {
-    for (l = 1; l <= k; l++)
-    {
-      P=P+h*lambda*lambda*SigmaR((k*h),(h*l))*GK[l][i];
-    }
-
-    I4=P+(h/2.0)*lambda*lambda*SigmaR((k*h),(i*h))*GK[i][i];
-    P=0.0;
-
-        for (l = 1; l < i; l++)
-        {
-          P = P + h*lambda*lambda*SigK[k][l]*conjf(GR[i+1][l]);
-        }
 
 
-    I5=P+(h/2.0)*lambda*lambda*SigK[k][k]*conjf(GR[i+1][k]);
-    P=0.0;
+          for (l = 1; l <= k; l++)
+          {
+            P=P+h*lambda*lambda*SigmaR((k*h),(h*l))*GK[l][i+1];
+          }
 
-    GK[k+1][i+1]= I*GzeroR(epsilon,(k*h)+h,(k*h))*GK[k][i]+(h/2.0)*GzeroR(epsilon,(k*h)+h,(k*h))*(I4+I5);
+          I4=P+(h/2.0)*lambda*lambda*SigmaR((k*h),(i*h))*GK[i][i+1]+(h/2.0)*lambda*lambda*SigmaR((k*h),(k*h))*GK[k][i+1];
+          P=0.0;
+
+              for (l = 1; l < i; l++)
+              {
+                P = P + h*lambda*lambda*SigK[k][l]*conjf(GR[i+1][l]);
+              }
+
+
+          I5=P+(h/2.0)*lambda*lambda*SigK[k][k]*conjf(GR[i+1][k])+(h/2.0)*lambda*lambda*SigK[k][i+1]*conjf(GR[i+1][i+1]);
+          P=0.0;
+
+
+
+
+          for (l = 1; l <= k; l++)
+          {
+            P=P+h*lambda*lambda*SigmaR((k*h)+h,(h*l))*GK[l][i+1];
+          }
+
+          I2=P+(h/2.0)*lambda*lambda*SigmaR((k*h)+h,(i*h))*GK[i][i+1];
+          P=0.0;
+
+              for (l = 1; l < i; l++)
+              {
+                P = P + h*lambda*lambda*SigK[k+1][l]*conjf(GR[i+1][l]);
+              }
+
+
+          I3=P+(h/2.0)*lambda*lambda*SigK[k+1][i+1]*conjf(GR[i+1][i+1])+(h/2.0)*lambda*lambda*SigK[k+1][k+1]*conjf(GR[i+1][k+1]);
+          P=0.0;
+
+
+      GK[k+1][i+1]= (I*GzeroR(epsilon,(k*h)+h,(k*h))*GK[k][i+1]+(h/2.0)*GzeroR(epsilon,(k*h)+h,(k*h))*(I4+I5)+(h/2.0)*GzeroR(epsilon,(k*h)+h,(k*h)+h)*(I4+I5))/(1+I*(h/2.0)*lambda*lambda*SigmaR((k*h)+h,(k*h)+h));
 
 
   }
@@ -236,7 +278,7 @@ Put K independent printf statements after this.
 
 
 for (i = 0; i < n; i++){
-printf("%f\t%f\t%f\n", i*h , 0.5*(crealf(GK[i][i])+1), 0.5*(sqrt(cimagf(GK[i][i])*cimagf(GK[i][i])-crealf(GK[i][i])*crealf(GK[i][i]))+1) )  ;
+printf("%f\t%f\n", i*h , 0.5*(cimagf(GK[i][i])+1) )  ;
 }
 
 }
